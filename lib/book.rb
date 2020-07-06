e9f9efe0628e81ef8ed2d87de189722a4e432a84
Cameron Hudson <camnn3@yahoo.com>
2020-07-06 16:31:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
-
+class Book
+  attr_accesspr :author, :page_count, :genre
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
