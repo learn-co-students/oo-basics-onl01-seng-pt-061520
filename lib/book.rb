b91012b7840989d6d67e8df94cabb059db2c7263
Michael R <mruhaak@gmail.com>
2020-07-10 02:26:06 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
 
-
+  def initialize(title)
+    @title = title
+  end
+#Page turn method coming up
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+end
+#learn spec/01_book_spec.rb
+#learn spec/02_shoe_spec.rb
